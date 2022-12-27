fc90a0310e6c046a64d0acea48f9f6711cbb69fe
Michael Rittmeister <michael@rittmeister.in>
2022-12-27 16:07:59 +0100

    Add button to request battery optimization exception

--- app/src/main/java/dev/schlaubi/icetracker/MainActivity.kt
+++ app/src/main/java/dev/schlaubi/icetracker/MainActivity.kt
@@ -1,11 +1,14 @@
 package dev.schlaubi.icetracker
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.PowerManager
 import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -13,15 +16,20 @@ import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import dev.schlaubi.icetracker.fetcher.Journey
@@ -142,10 +150,12 @@ private fun Context.startTracker() {
     )
 }
 
+@SuppressLint("BatteryLife") // unavoidable
 @Composable
 private fun ActionDropDown() {
-    var expanded by remember { mutableStateOf(false) }
     val context = LocalContext.current
+    var expanded by remember { mutableStateOf(false) }
+    var askForBatteryPermission by remember { mutableStateOf(!isIgnoringPowerOptimizations(context)) }
     val fileRequester = rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) {
         val uri = it ?: return@rememberLauncherForActivityResult
         context.contentResolver.openInputStream(uri)!!.use { stream ->
@@ -163,7 +173,16 @@ private fun ActionDropDown() {
         Toast.makeText(context, "Will be available after Restart", Toast.LENGTH_SHORT).show()
     }
 
-    IconButton(onClick = { expanded = true }) {
+    IconButton(onClick = { expanded = true }, modifier = Modifier.wrapContentSize()) {
+        if (askForBatteryPermission) {
+            Box(
+                Modifier
+                    .size(6.dp)
+                    .offset(8.dp, (-13).dp)
+                    .clip(CircleShape)
+                    .background(Color.Red)
+            )
+        }
         Icon(
             imageVector = Icons.Filled.MoreVert,
             contentDescription = stringResource(R.string.options)
@@ -180,5 +199,34 @@ private fun ActionDropDown() {
 
             context.startActivity(Intent(context, OssLicensesMenuActivity::class.java))
         })
+
+        if (askForBatteryPermission) {
+            DropdownMenuItem(
+                text = {
+                    Box(
+                        Modifier
+                            .size(6.dp)
+                            .offset(183.dp, (-3).dp)
+                            .clip(CircleShape)
+                            .background(Color.Red)
+                    )
+                    Text(stringResource(R.string.request_battery_permission))
+                },
+                onClick = {
+                    val permissionIntent =
+                        Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS).apply {
+                            data = Uri.parse("package:${BuildConfig.APPLICATION_ID}")
+                        }
+                    context.startActivity(permissionIntent)
+                    expanded = false; askForBatteryPermission = false
+                })
+        }
     }
+}
+
+
+private fun isIgnoringPowerOptimizations(context: Context): Boolean {
+    val powerManager = context.getSystemService(Context.POWER_SERVICE) as PowerManager
+
+    return powerManager.isIgnoringBatteryOptimizations(BuildConfig.APPLICATION_ID)
 }
